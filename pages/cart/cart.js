f07ab2040d10dee557304620a1a7d49ee01df3ae
1102298825@qq.com <111111aaa>
2017-01-19 17:56:23 +0800

    购物车页面开发

--- pages/cart/cart.js
+++ pages/cart/cart.js
@@ -10,13 +10,25 @@ Page({
 		        desc: '这是一份好吃的土豆烧鸡饭这是一份好吃的土豆烧鸡饭`',
 		        imgUrl: '/static/image/goods/good-template.jpg',
 		        price: 32,
-		        num: 1
+		        num: 1,
+						checked: false,
+						name: 2,
+						value: 1
 					}
-				]
+				],
+				shop:{
+					shopIcon:'https://gtms04.alicdn.com/tps/i4/TB1x4ExHVXXXXalXFXXSutbFXXX.jpg',
+					value: 1,
+					checked: false,
+					name: '烧烤专卖'
+				}
 			}
 		]
 	}, 
   onLoad: function () {
   	console.log('data', this.data);
   },
+	checkboxChange: function(e) {
+		console.log('checkbox发生change事件，携带value值为：', e.detail.value)
+	}
 });
